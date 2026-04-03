ab07ab19acbaca1684029bc187859052337f7c87
void <void@gmail.com>
2026-04-03 09:58:53 +0700

    fix z

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,7 @@ set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
 # Java JDK
 set -U fish_user_paths /usr/lib/jvm/java-17-openjdk/bin $fish_user_paths
+zoxide init fish | source
 
 # ----------------------
 # Greeting
@@ -27,7 +28,7 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
-alias unlock="sudo rm /var/lib/pacman/db.lck"
+alias unlock="sudo rm -i /var/lib/pacman/db.lck"
 alias fasmdoc="jvim -R /usr/share/doc/fasm/fasm.txt"
 #arcolinux logout unlock
 alias rmlogoutlock="sudo rm /tmp/arcologout.lock"
@@ -40,7 +41,6 @@ alias q='exit'
 #free
 #
 alias nznginx="sudo /usr/local/nginx/sbin/nginx"
-alias y="yazi"
 alias v="yazi"
 alias nznginxrc="sudo nvim /usr/local/nginx/conf/nginx.conf"
 alias gdb="gdb -x ~/c_gdb_init/.gdbinit "
@@ -70,33 +70,39 @@ alias d="ddgr"
 # Editor
 # ----------------------
 set -gx EDITOR nvim
+
 # ----------------------
-# PATHs
+# PKG_CONFIG_PATH
 # ----------------------
-# NPM global
-set -gx PATH $HOME/.npm-global/bin $PATH
+set -gx PKG_CONFIG_PATH \
+    ~/opt/SDL3/lib64/pkgconfig \
+    ~/opt/raylib5.5/lib64/pkgconfig \
+    $PKG_CONFIG_PATH
 
-# SDL3 & Raylib pkgconfig
-set -gx PKG_CONFIG_PATH ~/opt/SDL3/lib64/pkgconfig/ ~/opt/raylib5.5/lib64/pkgconfig/ $PKG_CONFIG_PATH
+# ----------------------
+# PATH (clean with fish_add_path)
+# ----------------------
 
-# Local bin directories
-set -gx PATH bin ~/bin ~/.local/bin $PATH
+# npm global
+fish_add_path ~/.npm-global/bin
 
-# NodeJS local bin
-set -gx PATH node_modules/.bin $PATH
+# local bins
+fish_add_path ~/bin
+fish_add_path ~/.local/bin
+
+# node_modules (project-local)
+fish_add_path node_modules/.bin
 
 # Go
 set -g GOPATH $HOME/go
-set -gx PATH $GOPATH/bin $PATH
+fish_add_path $GOPATH/bin
 
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH $PNPM_HOME $PATH
-end
+fish_add_path $PNPM_HOME
 
 # bun
 set -gx BUN_INSTALL "$HOME/.bun"
-set -gx PATH $BUN_INSTALL/bin $PATH
+fish_add_path $BUN_INSTALL/bin
 # opencode
 fish_add_path /home/nz/.opencode/bin
