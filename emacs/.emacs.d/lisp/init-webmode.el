84432621ef1d5e1c17b5dd920b904b685d13250b
void <void@gmail.com>
2026-05-01 21:15:41 +0700

    fix eglot

---
+++ emacs/.emacs.d/lisp/init-webmode.el
@@ -0,0 +1,14 @@
+(use-package web-mode
+  :ensure t
+  :mode
+  (("\\.phtml\\'" . web-mode)
+   ("\\.php\\'" . web-mode)
+   ("\\.tpl\\'" . web-mode)
+   ("\\.[agj]sp\\'" . web-mode)
+   ("\\.as[cp]x\\'" . web-mode)
+   ("\\.erb\\'" . web-mode)
+   ("\\.mustache\\'" . web-mode)
+   ("\\.djhtml\\'" . web-mode)))
+
+
+(provide 'init-webmode)
